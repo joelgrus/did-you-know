e01fbd6f9523b7b25ecf66d93edfcda5f148de41
Joel Grus <joelgrus@gmail.com>
2013-11-10 06:25:42 -0800

    added more dyks

--- did-you-know.js
+++ did-you-know.js
@@ -2,22 +2,23 @@
     var didYouKnows = [
       "The only English word that's spelled the same forwards and backwards is <b>indefatigable</b>.",
       "More than 99% of people are left-handed, right-handed, or ambidextrous.",
-      "The only English words that end in 'mt' are <b>dreamt</b> and <b>klimt</b>.",
+      "The only English words that end in 'mt' are <b>dreamt</b>, <b>breamt</b>, and <b>klimt</b>.",
+      "There are only two English words that both begin and end with 'und': <b>underground</b> and <b>und</b>.",
       "If you say the alphabet silently, every letter will sound exactly the same.",
       "The most Australian city in Australia is <b>Melbourne</b>.",
       "Goldfish actually contain very little gold.",
       "Lightning sometimes <b>does</b> strike twice in the same place, but never in the exact same <b>way</b>.",
       "The only U.S. state with no active volcanoes is Indiana.",
-      "The sentence 'See Spot run' uses every letter in the English language.",
+      "The shortest English sentence that uses every letter in the alphabet is <b>'See Spot run.'</b>.",
       "The average human brain contains <b>more than ten</b> Michael Jackson songs.",
       "You burn more calories climbing stairs than watching TV.",
-      "The world's smallest ocean is located in Omaha, Nebraska.",
+      "The world's smallest ocean is located in <b>Omaha, Nebraska</b>.",
       "Cows are the world's only remaining source of beef.",
       "A group of owls is called a <b><em>Smörgåsbord</em></b>.",
       "In space, no one can hear you scream.",
       "The 'WD' in WD-40 stands for <b>Walking Dead</b>.",
       "Americans read over <b>100</b> newspapers a day.",
-      "The average person laughs 10 times a year.",
+      "The average person laughs <b>11</b> times a year.",
       "Sharks cannot be killed.",
       "The 'DC' in Washington, DC stands for <b>Doctor of Chiropractic</b>.",
       "The Dead Sea is neither <b>dead</b> nor a <b>sea</b>.",
@@ -30,7 +31,11 @@
       "The longest English word without any vowels is <b>skwlrvthmngst</b>.",
       "Fear of the Internet is called <b>httphobia</b>.",
       "Albert Einstein never learned how to send a text message.",
-      "Months that begin on a Sunday will always have a <b>Monday the 2nd</b>."  
+      "Months that begin on a Sunday will always have a <b>Monday the 2nd</b>.",
+      "'Pablo' is the Spanish equivalent of the English name 'Pabl'.",
+      "The smallest natural number that cannot be described in less than twenty words is <b>seventeen</b>.",
+      "There is an island whose inhabitants always tell the truth.",
+      
     ];
 
     $('#did-you-know').html(didYouKnows[Math.floor(Math.random()*didYouKnows.length)]);
