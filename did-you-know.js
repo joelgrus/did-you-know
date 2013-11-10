73bc9dd310b12cb630bdae651997f0d4bcd20105
Joel Grus <joelgrus@gmail.com>
2013-11-09 22:52:36 -0800

    init

---
+++ did-you-know.js
@@ -0,0 +1,38 @@
+(function() { 
+    var didYouKnows = [
+      "The only English word that's spelled the same forwards and backwards is <b>indefatigable</b>.",
+      "More than 99% of people are left-handed, right-handed, or ambidextrous.",
+      "The only English words that end in 'mt' are <b>dreamt</b> and <b>klimt</b>.",
+      "If you say the alphabet silently, every letter will sound exactly the same.",
+      "The most Australian city in Australia is <b>Melbourne</b>.",
+      "Goldfish actually contain very little gold.",
+      "Lightning sometimes <b>does</b> strike twice in the same place, but never in the exact same <b>way</b>.",
+      "The only U.S. state with no active volcanoes is Indiana.",
+      "The sentence 'See Spot run' uses every letter in the English language.",
+      "The average human brain contains <b>more than ten</b> Michael Jackson songs.",
+      "You burn more calories climbing stairs than watching TV.",
+      "The world's smallest ocean is located in Omaha, Nebraska.",
+      "Cows are the world's only remaining source of beef.",
+      "A group of owls is called a <b><em>Smörgåsbord</em></b>.",
+      "In space, no one can hear you scream.",
+      "The 'WD' in WD-40 stands for <b>Walking Dead</b>.",
+      "Americans read over <b>100</b> newspapers a day.",
+      "The average person laughs 10 times a year.",
+      "Sharks cannot be killed.",
+      "The 'DC' in Washington, DC stands for <b>Doctor of Chiropractic</b>.",
+      "The Dead Sea is neither <b>dead</b> nor a <b>sea</b>.",
+      "Most snakes cannot read.",
+      "The hardest substance in the human body is <b>spite</b>.",
+      "The word 'fan' is short for <b>fantasmagoria</b>.",
+      "Out of the 250+ species of humans only 18 are known to be dangerous to sharks.",
+      "The only thing that can destroy a diamond is <b>heartbreak</b>.",
+      "Portugal is the only country that is also a continent.",
+      "The longest English word without any vowels is <b>skwlrvthmngst</b>.",
+      "Fear of the Internet is called <b>httphobia</b>.",
+      "Albert Einstein never learned how to send a text message.",
+      "Months that begin on a Sunday will always have a <b>Monday the 2nd</b>."  
+    ];
+
+    $('#did-you-know').html(didYouKnows[Math.floor(Math.random()*didYouKnows.length)]);
+    })();
+
